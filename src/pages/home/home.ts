cb3f31a75537bb11eca88f3a9792818cd455dd43
Flavio Seixas <fseixas@midiacom.uff.br>
2018-05-30 23:17:01 -0300

    v1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,11 +17,10 @@ export class HomePage {
   data: any;
 
   questions: any;
-  possibleAnswers: any;
-  selectedItem: any;
-  selectedItemIdx: any;
+  uiPossibleAnswers: any;
 
   btnSalvarEnable = false;
+  btnRetornarEnable = false;
 
   constructor(public navCtrl: NavController, public engine: RulesEngineProvider) {
 
@@ -32,7 +31,7 @@ export class HomePage {
     this.cardContent = content;
   }
 
-  private buildPage(data, selectedItemIdx) {
+  private buildPage(data) {
 
     if (Object.getOwnPropertyNames(data)[0] === "question") {
 
@@ -42,21 +41,20 @@ export class HomePage {
       this.data = data;
       var text = data["question"][this.id]["text"];
 
-      this.possibleAnswers = new Array();
+      this.uiPossibleAnswers = new Array();
       var selected = null;
       for(var p in data["question"][this.id]["valid_values"]) {
-        if (p === this.selectedItemIdx) {
+        if (data["question"][this.id]["valid_values"][p] === this.data["question"][this.id]["answer"]) {
           selected = true;
         }
         else {
           selected = false;
         }
 
-        console.log(Object.assign({},{"text":data["question"][this.id]["valid_values_text"][p]},{"selected":selected},{"hasAnswered":selected}));
-        this.possibleAnswers.push(Object.assign({},{"text":data["question"][this.id]["valid_values_text"][p]},{"selected":selected},{"hasAnswered":selected}));
+        this.uiPossibleAnswers.push(Object.assign({},{"text":data["question"][this.id]["valid_values_text"][p]},{"selected":selected},{"hasAnswered":selected}));
       };
 
-      if (selectedItemIdx === null) {
+      if (this.data["question"][this.id]["answer"] === "null") {
         this.btnSalvarEnable = false;
       }
 
@@ -69,7 +67,7 @@ export class HomePage {
       this.id = Object.getOwnPropertyNames(data["decision"])[0];
       var text = data["decision"][this.id]["text"];
 
-      this.possibleAnswers = new Array();
+      this.uiPossibleAnswers = new Array();
       this.updateCardContent("Decisão", text);
     }
   }
@@ -79,19 +77,35 @@ export class HomePage {
 
     this.engine.getNewEvent().subscribe(data => {
       this.questions.push(data);
-      this.buildPage(data, null);
+      
+      if (this.questions.length > 1) {
+        console.log("passei aqui também");
+        this.btnRetornarEnable = true;
+      }
+      
+      this.buildPage(data);
     });
   }
 
   public selectAnswer(index) {
-    this.selectedItem = this.data["question"][this.id]["valid_values"][index];
-    this.selectedItemIdx = index;
+    this.questions[this.questions.length-1]["question"][this.id]["answer"] = this.data["question"][this.id]["valid_values"][index];
     this.btnSalvarEnable = true;
   }
 
   public sendAnswer() {
+    this.engine.setFact(this.id, this.questions[this.questions.length-1]["question"][this.id]["answer"]);
+  }
+
+  public moveBack() {
+    this.questions.pop();
+    this.id = Object.getOwnPropertyNames(this.questions[this.questions.length-1]["question"])[0];
     console.log(this.id);
-    console.log(this.selectedItem);
-    this.engine.setFact(this.id, this.selectedItem);
+    if (this.questions.length <= 1) {
+      console.log("passei aqui");
+      this.btnRetornarEnable = false;
+    }
+    
+    this.engine.setFact(this.id, "null");
+
   }
 }
